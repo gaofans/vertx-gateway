fea6d4053902e2441b230ded659fc81bd1399b5a
GaoFans <123456>
2021-02-19 22:39:14 +0800

    init

---
+++ vertx-gateway-server/src/main/java/com/gaofans/vertx/gateway/Application.java
@@ -0,0 +1,15 @@
+package com.gaofans.vertx.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author GaoFans
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
